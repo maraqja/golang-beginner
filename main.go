40fdb362614d057b8b05145b13de10a5a778abbb
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-13 13:04:21 +0300

    variable creating

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Printf("Hello world")
+	var userHeight = 1.8          // неявно присваивается float64 (выведение типов)
+	var userKg = 100              // неявно присваивается int (выведение типов)
+	var IMT = userKg / userHeight // нельзя разделить - в go нет неявного преобразования
 }
