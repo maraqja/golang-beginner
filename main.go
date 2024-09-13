394946634847b213148d01c3ccab7449162641aa
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-13 13:35:03 +0300

    work with consts

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
+	const IMTPower float64 = 2 // тк функция Pow требует степень в float64
+
 	userHeight := 1.8 // при таком способе объявления переменной нельзя указать тип (всегда будет неявное присвоение) и создать пустую переменную (без присвоения значения, просто создать переменную)
 	var userKg float64
 	userKg = 100
-	IMT := userKg / math.Pow(userHeight, 2) // неявно преобразуется в float64
+	IMT := userKg / math.Pow(userHeight, IMTPower) // неявно преобразуется в float64
 	fmt.Print(IMT)
 }
