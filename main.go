8ae50645b6edac4bdbcdcdc5c394d7739f85555a
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-13 13:15:55 +0300

    work with types

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	var userHeight = 1.8          // неявно присваивается float64 (выведение типов)
-	var userKg = 100              // неявно присваивается int (выведение типов)
-	var IMT = userKg / userHeight // нельзя разделить - в go нет неявного преобразования
+	var userHeight = 1.8 // неявно присваивается float64 (выведение типов)
+	var userKg float64 = 100
+	var IMT = userKg / math.Pow(userHeight, 2) // неявно преобразуется в float64
+	fmt.Print(IMT)
 }
