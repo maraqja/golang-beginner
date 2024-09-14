528c9ddb10fa2473704710a4d0c366958f6a384c
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-14 18:56:30 +0300

    function with returning

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	const IMTPower float64 = 2 // тк функция Pow требует степень в float64
 
 	var userHeight float64 // 0.0 если не указать (по умолчанию)
 	var userKg float64     // 0.0 если не указать (по умолчанию)
@@ -16,12 +15,19 @@ func main() {
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес (в кг):")
 	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeight/100, IMTPower) // неявно преобразуется в float64
 
+	IMT := calculateIMT(userKg, userHeight)
 	outputResult(IMT)
 }
 
 func outputResult(imt float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.f", imt) // кладем форматированную строку в переменную
 	fmt.Print((result))
+}
+
+func calculateIMT(userKg float64, userHeight float64) float64 { // объявляем типы входных данных и тип результата
+	const IMTPower float64 = 2 // тк функция Pow требует степень в float64
+
+	IMT := userKg / math.Pow(userHeight/100, IMTPower) // неявно преобразуется в float64
+	return IMT
 }
