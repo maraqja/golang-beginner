a4fee18d2c478822ac3ec53c6a0af59a172733e0
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-14 18:38:01 +0300

    sprintf

--- main.go
+++ main.go
@@ -11,11 +11,14 @@ func main() {
 	var userHeight float64 // 0.0 если не указать (по умолчанию)
 	var userKg float64     // 0.0 если не указать (по умолчанию)
 	userKg = 100
-	fmt.Print(`Калькулятор индекса массы тела
-Введите свой рост (в сантиметрах):`)
+	fmt.Println(`Калькулятор индекса массы тела`)
+	fmt.Print("Введите свой рост (в сантиметрах):")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес (в кг):")
 	fmt.Scan(&userKg)
 	IMT := userKg / math.Pow(userHeight/100, IMTPower) // неявно преобразуется в float64
-	fmt.Printf("Ваш индекс массы тела: %.f", IMT)
+	fmt.Printf("Ваш индекс массы тела: %.f\n", IMT)
+
+	result := fmt.Sprintf("Ваш индекс массы тела: %.f", IMT) // кладем форматированную строку в переменную
+	fmt.Print((result))
 }
