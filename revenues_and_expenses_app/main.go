0a1a40066650938625caf3be86891d118c94bed9
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-19 20:02:24 +0300

    array and slice difference + len and cap

--- revenues_and_expenses_app/main.go
+++ revenues_and_expenses_app/main.go
@@ -6,26 +6,27 @@ func main() {
 
 	transactions := [5]int{5, 10, -7, 4, 5} // 3 - размер массива, то что в {} - элементы массива
 
-	// // Другой способ объявления массива
-	// var transactions [3]int
-	// transactions[0] = 5
-	// transactions[1] = 10
-	// transactions[2] = -7
+	transactions_new := transactions // ТУТ ПРОИСХОДИТ ПОЛНОЕ КОПИРОВАНИЕ !!! (НЕ ПО ССЫЛКЕ КАК В JS)
+	transactions_new[0] = 200        // ЭТО НЕ ИЗМЕНИТ ИСХОДНЫЙ МАССИВ
 
-	// // // Объявление пустого массива нужного размера
-	// transactions := [3]int{} // 3 - размер массива, то что в {} - элементы массива
-	// transactions[0] = 1
+	transactions_slice := transactions[0:4] // СЛАЙС ССЫЛАЕТСЯ НА МАССИВ !!!
+	transactions_slice[0] = 0               // ЭТО ИЗМЕНИТ ИСХОДНЫЙ МАССИВ
 
-	fmt.Println(transactions[1])
+	transactions_slice_new := transactions_slice[0:3] // СЛАЙС ОТ СЛАЙСА ВСЕ РАВНО ССЫЛАЕТСЯ НА ИСХОДНЫЙ МАССИВ
+	transactions_slice_new[2] = 100                   // ИЗМЕНИТ В ИСХОДНОМ МАССИВЕ ТАКЖЕ (ну и в слайсе предыдущем тоже)
 
-	banks := [2]string{"t", "sber"}
-	fmt.Println(banks)
-	banks[0] = "tinkof"
+	fmt.Println(transactions)
+	fmt.Printf("new Array: %v\n", transactions_new)
+	fmt.Printf("Slice from array: %v\n", transactions_slice)
+	fmt.Printf("Slice from slice: %v\n", transactions_slice_new)
 
-	// partial_slice := transactions[0:5] // с какого элемента ВКЛЮЧИТЕЛЬНО до какого элемента НЕВКЛЮЧИТЕЛЬНО (или можно понимать как кол-во элементов)
-	// partial_slice := transactions[:2] // берем 2 элемента с начала
-	partial_slice := transactions[2:] // берем все элементы, после элемента под индексом 2 включительно
+	// len (length) - размер слайса
+	// cap (capacity) - сколько есть места для элементов в слайсе справа
+	fmt.Printf("For slice from array:\nlen: %v\ncap: %v\n", len(transactions_slice), cap(transactions_slice))
+	fmt.Printf("For slice from slice:\nlen: %v\ncap: %v\n", len(transactions_slice_new), cap(transactions_slice_new))
 
-	fmt.Println(partial_slice)
-
+	// за счет cap (capacity) можем расширить слайс вправо в рамках размера исхдного массива
+	// то есть можно увеличить слайс вправо, но влево - нельзя
+	transactions_slice_new = transactions_slice_new[0:5]
+	fmt.Printf("Slice from slice: %v\n", transactions_slice_new) // получаем расширенный из самого себя слайс за счет существования capacity
 }
