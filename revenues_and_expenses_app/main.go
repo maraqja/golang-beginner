ed2d5e59184db7eba1eb22bd0622fe08cc2a964a
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-19 20:46:05 +0300

    dynamic array (it is slice)

--- revenues_and_expenses_app/main.go
+++ revenues_and_expenses_app/main.go
@@ -3,30 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	// Динамический массив - массив, которому не задан размер
+	transactions := []int{5, 10, -7, 4, 5} // создаем массив без ограничения по длине (по факту это слайс под капотом, хранящий ссылку на массив (хз какой))
+	slice := transactions                  // тут по сути копируем слайс => изменение в slice изменит исходный слайс (который типо динамический массив)
+	slice[0] = 0                           // изменится в transactions
 
-	transactions := [5]int{5, 10, -7, 4, 5} // 3 - размер массива, то что в {} - элементы массива
-
-	transactions_new := transactions // ТУТ ПРОИСХОДИТ ПОЛНОЕ КОПИРОВАНИЕ !!! (НЕ ПО ССЫЛКЕ КАК В JS)
-	transactions_new[0] = 200        // ЭТО НЕ ИЗМЕНИТ ИСХОДНЫЙ МАССИВ
-
-	transactions_slice := transactions[0:4] // СЛАЙС ССЫЛАЕТСЯ НА МАССИВ !!!
-	transactions_slice[0] = 0               // ЭТО ИЗМЕНИТ ИСХОДНЫЙ МАССИВ
-
-	transactions_slice_new := transactions_slice[0:3] // СЛАЙС ОТ СЛАЙСА ВСЕ РАВНО ССЫЛАЕТСЯ НА ИСХОДНЫЙ МАССИВ
-	transactions_slice_new[2] = 100                   // ИЗМЕНИТ В ИСХОДНОМ МАССИВЕ ТАКЖЕ (ну и в слайсе предыдущем тоже)
-
-	fmt.Println(transactions)
-	fmt.Printf("new Array: %v\n", transactions_new)
-	fmt.Printf("Slice from array: %v\n", transactions_slice)
-	fmt.Printf("Slice from slice: %v\n", transactions_slice_new)
-
-	// len (length) - размер слайса
-	// cap (capacity) - сколько есть места для элементов в слайсе справа
-	fmt.Printf("For slice from array:\nlen: %v\ncap: %v\n", len(transactions_slice), cap(transactions_slice))
-	fmt.Printf("For slice from slice:\nlen: %v\ncap: %v\n", len(transactions_slice_new), cap(transactions_slice_new))
-
-	// за счет cap (capacity) можем расширить слайс вправо в рамках размера исхдного массива
-	// то есть можно увеличить слайс вправо, но влево - нельзя
-	transactions_slice_new = transactions_slice_new[0:5]
-	fmt.Printf("Slice from slice: %v\n", transactions_slice_new) // получаем расширенный из самого себя слайс за счет существования capacity
+	fmt.Printf("Dynamic array (it is slice !!!): %v\n", transactions)
+	fmt.Printf("Slice from dynamic array: %v\n", slice)
+	// slice[6] = 200                         // будет вызывать ошибку, тк нет такого индекса в массиве
+	// нужно использовать append для вставки новых элементов
+	transactions = append(transactions, 100) // append можно использовать только на slice-ах
+	// ВАЖНО - ссылающийся slice не изменится: APPEND создает новый массив в памяти, slice будет ссылаться на старые данные transactions
+	fmt.Printf("Dynamic array (it is slice !!!): %v\n", transactions)
+	fmt.Printf("Slice from dynamic array: %v\n", slice)
 }
