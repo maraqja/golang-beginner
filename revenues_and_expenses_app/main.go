f30f04bca3c5f597fef92378c62fb07b87100653
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-19 19:24:58 +0300

    array declaration other ways + get/change element using index

--- revenues_and_expenses_app/main.go
+++ revenues_and_expenses_app/main.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 
 func main() {
+
 	transactions := [3]int{5, 10, -7} // 3 - размер массива, то что в {} - элементы массива
-	// banks := [2]string{"t", "sber"}
-	fmt.Println(transactions)
+
+	// // Другой способ объявления массива
+	// var transactions [3]int
+	// transactions[0] = 5
+	// transactions[1] = 10
+	// transactions[2] = -7
+
+	// // // Объявление пустого массива нужного размера
+	// transactions := [3]int{} // 3 - размер массива, то что в {} - элементы массива
+	// transactions[0] = 1
+
+	banks := [2]string{"t", "sber"}
+
+	banks[0] = "tinkof"
+	fmt.Println(transactions[1])
+	fmt.Println(banks)
 }
