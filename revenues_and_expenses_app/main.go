54000ac094461aee71a270ca80c75733bdba9903
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-19 19:38:59 +0300

    array slices

--- revenues_and_expenses_app/main.go
+++ revenues_and_expenses_app/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	transactions := [3]int{5, 10, -7} // 3 - размер массива, то что в {} - элементы массива
+	transactions := [5]int{5, 10, -7, 4, 5} // 3 - размер массива, то что в {} - элементы массива
 
 	// // Другой способ объявления массива
 	// var transactions [3]int
@@ -16,9 +16,16 @@ func main() {
 	// transactions := [3]int{} // 3 - размер массива, то что в {} - элементы массива
 	// transactions[0] = 1
 
-	banks := [2]string{"t", "sber"}
-
-	banks[0] = "tinkof"
 	fmt.Println(transactions[1])
+
+	banks := [2]string{"t", "sber"}
 	fmt.Println(banks)
+	banks[0] = "tinkof"
+
+	// partial_slice := transactions[0:5] // с какого элемента ВКЛЮЧИТЕЛЬНО до какого элемента НЕВКЛЮЧИТЕЛЬНО (или можно понимать как кол-во элементов)
+	// partial_slice := transactions[:2] // берем 2 элемента с начала
+	partial_slice := transactions[2:] // берем все элементы, после элемента под индексом 2 включительно
+
+	fmt.Println(partial_slice)
+
 }
