a82568ab3a8d49c720ddcd4a38e906bcbac13e28
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-20 19:39:17 +0300

    cycle for array (range)

--- revenues_and_expenses_app/main.go
+++ revenues_and_expenses_app/main.go
@@ -4,10 +4,6 @@ import "fmt"
 
 func main() {
 
-	tr1 := []int{1, 2, 3}
-	tr2 := []int{4, 5, 6}
-	tr1 = append(tr1, tr2...) // unpack (~ spread в js)
-	fmt.Println(tr1)
 	// Упражнение
 	var transactions []float64
 
@@ -20,5 +16,9 @@ func main() {
 		}
 		transactions = append(transactions, transaction)
 	}
-	fmt.Printf("Transactions: %v", transactions)
+	// fmt.Printf("Transactions: %v", transactions)
+
+	for index, value := range transactions {
+		fmt.Println(index, value)
+	}
 }
