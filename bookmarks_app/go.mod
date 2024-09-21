8da39c5379e4898096e7143c735b9e3663b0ca60
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-21 15:27:36 +0300

    map declaration

---
+++ bookmarks_app/go.mod
@@ -0,0 +1,3 @@
+module bookmarks_app
+
+go 1.23.1
---
+++ bookmarks_app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//map = key - value
+	m := map[string]string{ // [тип ключа, тип значения]
+		"PurpleSchool": "https://purpleschool.ru",
+	}
+	fmt.Println(m)
+}
