f1f74f73a1d32135238170acdb14f0fb7921dee2
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-21 17:16:42 +0300

    delete returning because it is pointer (ссылочный) type

--- bookmarks_app/main.go
+++ bookmarks_app/main.go
@@ -5,13 +5,6 @@ import "fmt"
 type stringMap = map[string]string
 
 func main() {
-	m := make(stringMap, 2)
-	m["A"] = "1"
-	m["B"] = "2"
-	m["C"] = "3" // на этот уже будет выделяться память при добавлении
-	fmt.Println(m)
-	fmt.Println(len(m)) // у мапа нет capacity
-
 	// var bookmarks map[string]string
 	bookmarks := stringMap{}
 	fmt.Println("Закладки")
@@ -23,9 +16,9 @@ menu:
 		case 1:
 			getBookmarks(bookmarks)
 		case 2:
-			bookmarks = addBookmark(bookmarks)
+			addBookmark(bookmarks)
 		case 3:
-			bookmarks = deleteBookmark(bookmarks)
+			deleteBookmark(bookmarks)
 		case 4:
 			break menu // выходим именно из цикла, а не из case
 		}
@@ -52,20 +45,18 @@ func getBookmarks(bookmarks stringMap) {
 	}
 }
 
-func addBookmark(bookmarks stringMap) stringMap {
+func addBookmark(bookmarks stringMap) {
 	var new_bookmark_key, new_bookmark_value string
 	fmt.Println("Введите название: ")
 	fmt.Scan(&new_bookmark_key)
 	fmt.Println("Введите ссылку: ")
 	fmt.Scan(&new_bookmark_value)
 	bookmarks[new_bookmark_key] = new_bookmark_value
-	return bookmarks
 }
 
-func deleteBookmark(bookmarks stringMap) stringMap {
+func deleteBookmark(bookmarks stringMap) {
 	var bookmark_key string
 	fmt.Println("Введите название: ")
 	fmt.Scan(&bookmark_key)
 	delete(bookmarks, bookmark_key)
-	return bookmarks
 }
