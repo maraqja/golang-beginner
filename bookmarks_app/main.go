ac3ef8b4e75d2116c139211bc62070b9efd358cb
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-21 15:34:22 +0300

    standart actions with map

--- bookmarks_app/main.go
+++ bookmarks_app/main.go
@@ -8,4 +8,12 @@ func main() {
 		"PurpleSchool": "https://purpleschool.ru",
 	}
 	fmt.Println(m)
+	fmt.Println(m["PurpleSchool"]) // Читаем элемент мапа
+	m["PurpleSchool"] = "https://new.purpleschool.ru"
+	fmt.Println(m["PurpleSchool"])
+	m["Google"] = "https://google.com" // Добавляем элемент мапа
+	m["Yandex"] = "https://yandex.ru"
+	fmt.Println(m)
+	delete(m, "Yandex") // Удаляем элемент мапа
+	fmt.Println(m)
 }
