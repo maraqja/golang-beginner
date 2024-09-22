1afaa58fb22528d9ab6fa067106d415aff42586e
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-22 19:06:32 +0300

    validation and error handling in constructor-function

--- password_app/main.go
+++ password_app/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	"net/url"
 )
 
 type account struct { // Описываем тип стракта
@@ -11,13 +13,18 @@ type account struct { // Описываем тип стракта
 	url      string
 }
 
-func newAccount(login, password, url string) *account { // Это типо можно использовать как конструктор стракта (название просто по соглашению)
+func newAccount(login, password, urlString string) (*account, error) { // Это типо можно использовать как конструктор стракта (название просто по соглашению)
 	// Хорошо в случае если нужно валидировать входные данные
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		// return nil, err
+		return nil, errors.New("Invalid URL")
+	}
 	return &account{ // возвращаем именно ссылку на структуру, тк иначе мы создадим структуру в функции и еще потом скопириуем в переменную при вызове функции
 		login:    login,
 		password: password,
-		url:      url,
-	}
+		url:      urlString,
+	}, nil
 }
 
 // Указываем что функция outputPassword - метод struct account
@@ -40,7 +47,11 @@ func main() {
 	login := promptData("Введите логин")
 	url := promptData("Введите URL")
 
-	account1 := newAccount(login, "", url)
+	account1, error := newAccount(login, "", url)
+	if error != nil {
+		fmt.Print(error)
+		return
+	}
 	account1.generatePassword(10)
 	account1.outputPassword()
 	fmt.Println(account1.url)
