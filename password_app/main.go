948592185d5f0b85332a5d281e6af71665466e6f
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-09-22 18:27:00 +0300

    Упражнение: генерация пароля

--- password_app/main.go
+++ password_app/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
 
 type account struct { // Описываем тип стракта
 	login    string
@@ -8,22 +11,15 @@ type account struct { // Описываем тип стракта
 	url      string
 }
 
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_*!")
+
 func main() {
-
-	// str := "Привет!)"
-	str := []rune("Привет!") // Строку можно представить как массив рун
-	for _, ch := range str { // цикл по строке, ch имеет тип Rune
-		fmt.Println(ch, string(ch))
-	}
-	// Rune - юникод представления буквы (имеет тип int32)
-
 	login := promptData("Введите логин")
-	password := promptData("Введите пароль")
 	url := promptData("Введите URL")
 
 	account1 := account{ // Создаем стракта (инстас типа стракта)
 		login:    login,
-		password: password,
+		password: generatePassword(10),
 		url:      url,
 	}
 
@@ -32,7 +28,6 @@ func main() {
 	// 	password,
 	// 	url,
 	// }
-
 	outputPassword(&account1)
 
 }
@@ -47,4 +42,12 @@ func promptData(prompt string) string {
 
 func outputPassword(acc *account) { // struct полностью копируется при передаче в функцию, поэтому есть смысл передавать их по ссылке
 	fmt.Println((*acc).login, (*acc).password, acc.url) // GO позволяет использовать shortcode для dereference - вместо (*acc).url использовать просто acc.url
+}
+
+func generatePassword(n int) string {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	return string(res)
 }
